43e76c44999bb6e49894b28ac31dda1fda998e1e
ilyadrevov-dotcom <ilya.drevov@gmail.com>
2025-09-13 15:54:47 +0300

    do functions

---
+++ main.py
@@ -0,0 +1,23 @@
+
+#print(a+b)
+def summ():
+    a = 17
+    b = 20
+    #return a+b
+    print(a+b)
+#summ()
+def divide():
+    a = 7
+    b = 8
+    print(a/b)
+divide()
+def multiply():
+    a = 10
+    b = 20
+    print(a*b)
+multiply()
+def subtract():
+    a = 10
+    b = 20
+    print(a-b)
+subtract()
